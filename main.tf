003660b57d99332bca370ca2844f0c4e9e77591a
Shriyah <shriyahsk@gmail.com>
2026-03-11 12:29:01 +0000

    Update instance naming using locals

--- main.tf
+++ main.tf
@@ -40,6 +40,20 @@ ingress {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -48,10 +62,20 @@ ingress {
   }
 }
 
+# Instance names
+locals {
+  instance_names = [
+    "jenkins-server",
+    "k8s-master",
+    "k8s-worker"
+  ]
+}
+
 resource "aws_instance" "server" {
   count         = 3
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t3.micro"
+  key_name      = "terraform-key"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
