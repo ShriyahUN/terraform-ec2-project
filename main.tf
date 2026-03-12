c9b64ef982a2f3b5bcaa20c5dc7c761a5699e9a5
Shriyah <113592850+ShriyahUN@users.noreply.github.com>
2026-03-12 13:36:55 +0530

    Update main.tf
    increased root volume capacity and changed instance type

--- main.tf
+++ main.tf
@@ -13,34 +13,34 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-  from_port   = 8080
-  to_port     = 8080
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  from_port   = 3000
-  to_port     = 3000
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  from_port   = 9090
-  to_port     = 9090
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
+  ingress {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
@@ -62,19 +62,38 @@ ingress {
   }
 }
 
+locals {
+  instance_types = [
+    "c7i-flex.large",
+    "m7i-flex.large",
+    "c7i-flex.large"
+  ]
+
+  root_volume_sizes = [
+    40,
+    50,
+    40
+  ]
+}
 
 resource "aws_instance" "server" {
   count         = 3
   ami           = "ami-0b6c6ebed2801a5cb"
-  instance_type = "t3.micro"
+  instance_type = local.instance_types[count.index]
   key_name      = "terraform-key"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  root_block_device {
+    volume_size = local.root_volume_sizes[count.index]
+    volume_type = "gp3"
+  }
 
   tags = {
     Name = "terraform-server-${count.index}"
   }
 }
+
 output "public_ips" {
   value = aws_instance.server[*].public_ip
 }
